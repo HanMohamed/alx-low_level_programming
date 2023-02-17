f60ece87785a1d6988897a36753331eb49f3e9df
HanMohamed <hmohamed2@seattleu.edu>
2023-02-16 22:20:11 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int lowerChar = 97;
+
+	while (lowerChar <= 122)
+	{
+		putchar(lowerChar);
+		lowerChar++;
+	}
+	lowerChar = 10;
+	putchar(lowerChar);
+	return (0);
+}
