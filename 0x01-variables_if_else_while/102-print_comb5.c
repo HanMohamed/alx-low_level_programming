0fbe7b55e455661a59fa6e2d9670f085360cffe7
HanMohamed <hmohamed2@seattleu.edu>
2023-02-18 01:44:43 -0800

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -31,20 +31,17 @@ int main(void)
 			{
 				for (num4 = 48; num4 <= 57; num4++)
 				{
-					if (num1 < num3 || ( num1 == num3 && num2 <= num4))
+					if ((num1 < num3 || (num1 == num3 && num2 <= num4)) && !(num1 == num3 && num2 == num4))
 					{
-						if (!(num1 == num3 && num2 == num4))
+						putchar(num1);
+						putchar(num2);
+						putchar(space);
+						putchar(num3);
+						putchar(num4);
+						if (!(num1 == 57 && num2 == 56 && num3 == 57 && num4 == 57))
 						{
-							putchar(num1);
-							putchar(num2);
+							putchar(comma);
 							putchar(space);
-							putchar(num3);
-							putchar(num4);
-							if (!(num1 == 57 && num2 == 56 && num3 == 57 && num4 == 57))
-							{
-								putchar(comma);
-								putchar(space);
-							}
 						}
 					}
 				}
