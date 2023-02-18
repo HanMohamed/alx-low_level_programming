c94f071d2a031e580d6c018699c57e52ea8873f0
HanMohamed <hmohamed2@seattleu.edu>
2023-02-17 18:33:45 -0800

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+	int numL;
+	int numR;
+	int space;
+	int comma;
+	int nwLine;
+
+	space = 32;
+	comma = 44;
+	nwLine = 10;
+
+	for (numL = 48; numL <= 57; numL++)
+	{
+		for (numR = numL + 1; numR <= 57; numR++)
+		{
+			putchar(numL);
+			putchar(numR);
+			if (numL != 56)
+			{
+				putchar(comma);
+				putchar(space);
+			}
+		}
+	}
+	putchar(nwLine);
+	return (0);
+}
