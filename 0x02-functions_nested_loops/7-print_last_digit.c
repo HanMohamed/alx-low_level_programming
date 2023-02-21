60b198faa84ac63966feac04a387a20a09fc10fb
HanMohamed <hmohamed2@seattleu.edu>
2023-02-21 11:03:48 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,9 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	if (n < 0)
-		n *= -1;
 	lastDigit = n % 10;
+	if (lastDigit < 0)
+		lastDigit *= -1;
 	_putchar('0' + lastDigit);
 	return (lastDigit);
 }
