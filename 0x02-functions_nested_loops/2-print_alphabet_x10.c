2084ff092db22fd25656107b13f27abf7272cf86
HanMohamed <hmohamed2@seattleu.edu>
2023-02-21 09:18:01 -0800

    fix betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet.
+ * print_alphabet_x10 - print alphabet.
  *
  * Description: A function that prints 10 times the alphabet.
  * in lowercase, followed by a new line.
