d8d0a2b13d3e1280381a3fad65f53a174e29bff6
HanMohamed <hmohamed2@seattleu.edu>
2023-02-21 13:07:42 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,10 +18,13 @@ void times_table(void)
 		for (j = 0; j <= n; j++)
 		{
 			ntimes = i * j;
-			if (ntimes / 10 == 0)
-				_putchar(' ');
-			else
-				_putchar('0' + ntimes / 10);
+			if (ntimes != 0)
+			{
+				if (ntimes / 10 == 0)
+					_putchar(' ');
+				else
+					_putchar('0' + ntimes / 10);
+			}
 			_putchar('0' + ntimes % 10);
 			if (j != 9)
 			{
