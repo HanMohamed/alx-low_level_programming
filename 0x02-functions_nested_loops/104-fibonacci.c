a9a25df4235981040b2cda9a988fc32be738607c
HanMohamed <hmohamed2@seattleu.edu>
2023-02-21 19:10:58 -0800

    print_fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,14 +8,14 @@ void print_fib(void)
 {
 	int n = 98;
 	int i;
-	unsigned long int a = 0;
-	unsigned long int b = 1;
+	unsigned long int a = 1;
+	unsigned long int b = 2;
 	unsigned long int c;
 
 	for (i = 1; i <= n; i++)
 	{
 		c = a + b;
-		printf("%ld", c);
+		printf("%lu", c);
 		if (i != n)
 			printf(", ");
 		a = b;
