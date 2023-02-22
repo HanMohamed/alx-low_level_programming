57f8b8d322f4087dfcce797934b9830322d45070
HanMohamed <hmohamed2@seattleu.edu>
2023-02-21 18:58:43 -0800

    print_fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_fib - prints
+ */
+void print_fib(void)
+{
+	int n = 98;
+	int i;
+	long a = 0;
+	long b = 1;
+	long c;
+
+	for (i = 1; i <= n; i++)
+	{
+		c = a + b;
+		printf("%ld", c);
+		if (i != n)
+			printf(", ");
+		a = b;
+		b = c;
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_fib();
+
+	return (0);
+}
