ee0add76f271a2a92805025fe84719ec9de6f809
HanMohamed <hmohamed2@seattleu.edu>
2023-02-23 14:34:11 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,29 +8,22 @@
 void print_number(int n)
 {
 	int div = 1000000000;
-	int a1, a2;
-
-	if (n > 1000000000)
-	{
-		a1 = n / 1000000000;
-		a2 = n % 1000000000;
-		print_number(a1);
-		print_number(a2);
-	}
+	unsigned int un;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	while (div > 0 && n / div == 0)
+	un = n;
+	while (div > 0 && un / div == 0)
 		div /= 10;
 	if (div == 0)
-		_putchar('0' + n);
-	while (n > 0)
+		_putchar('0' + un);
+	while (un > 0)
 	{
-		_putchar('0' + (n / div));
-		n %= div;
+		_putchar('0' + (un / div));
+		un %= div;
 		div /= 10;
 	}
 }
