11f70b53d3dc872565fdb1e7f28ca42bc690f2b4
HanMohamed <hmohamed2@seattle.com>
2023-03-02 14:46:25 -0800

    latest 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,66 +11,33 @@
  *
  * Return: a pointer to the sum.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int get_len(char *p)
 {
-	char *p = n1;
-	int i, smalln, largen, diff, sum;
-	int len1 = 0;
-	int len2 = 0;
-	int carry = 0;
-	char *d = n2;
-	char *start = r;
-	char *end = r;
-	char temp;
-	char *large;
-	char *small;
+	int len = 0;
+	char *d = p;
 
-	while (*p != '\0')
-	{
-		p++;
-		len1++;
-	}
 	while (*d != '\0')
 	{
 		d++;
-		len2++;
+		len++;
 	}
-	if (len1 >= len2)
-	{
-		large = n1;
-		small = n2;
-		diff = len1 - len2;
-		smalln = len2;
-		largen = len1;
-	}
-	else
-	{
-		large = n2;
-		small = n1;
-		diff = len2 - len1;
-		smalln = len1;
-		largen = len2;
-	}
-	for (i = smalln - 1; i >= 0; i--)
-	{
-		sum = ((small[i] - '0') + (large[i + diff] - '0') + carry);
-		*end = (sum % 10 + '0');
-		carry = sum / 10;
+	return (len);
+}
+void swap_n2_n1(char *n1, char *n2)
+{
+	char *temp = n1;
+
+	n1 = n2;
+	n2 = temp;
+}
+void reverse(char *n1)
+{
+	char *start = n1;
+	char *end = n1;
+	char temp;
+
+	while (*end != '\0')
 		end++;
-	}
-	for (i = largen - smalln - 1; i >= 0; i--)
-	{
-		sum = (large[i] - '0' + carry);
-		*end = (sum % 10 + '0');
-		carry = sum / 10;
-		end++;
-	}
-	if (carry)
-	{
-		*end = (carry + '0');
-		largen++;
-		end++;
-	}
 	while (end > start)
 	{
 		end--;
@@ -79,8 +46,47 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		*end = temp;
 		start++;
 	}
-	r[largen] = '\0';
-	if (largen >= size_r)
+}
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, len1, len2, diff, sum, swapint;
+	int carry = 0;
+	char *p = r;
+
+	len1 = get_len(n1);/* larg */
+	len2 = get_len(n2);/*small */
+	if (len1 < len2)
+	{
+		swap_n2_n1(n1, n2);
+		swapint = len1;
+		len1 = len2;
+		len2 = swapint;
+	}
+	diff = len1 - len2;
+	for (i = len2 - 1; i >= 0; i--)
+	{
+		sum = ((n2[i] - '0') + (n1[i + diff] - '0') + carry);
+		*p = (sum % 10 + '0');
+		carry = sum / 10;
+		p++;
+	}
+	for (i = n1 - n2 - 1; i >= 0; i--)
+	{
+		sum = (n1[i] - '0' + carry);
+		*p = (sum % 10 + '0');
+		carry = sum / 10;
+		p++;
+	}
+	if (carry)
+	{
+		*p = (carry + '0');
+		len1++;
+		p++;
+	}
+	r[len1] = '\0';
+	reverse(r);
+	if (len1 >= size_r)
 		r = 0;
 	return (r);
 }
