394e48ee8d7dc811188fdac837c04220a2c520d5
HanMohamed <hmohamed2@seattle.com>
2023-03-01 22:36:16 -0800

    ==========

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - print integer
+ * You are not allowed to use long, arrays or pointers.
+ *@n: integer to print it.
+ */
+void print_number(int n)
+{
+	int div = 1000000000;
+	unsigned int un;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	un = n;
+	while (div > 0 && un / div == 0)
+		div /= 10;
+	if (div == 0)
+		_putchar('0' + un);
+	while (un > 0 || div > 0)
+	{
+		_putchar('0' + (un / div));
+		un %= div;
+		div /= 10;
+	}
+}
