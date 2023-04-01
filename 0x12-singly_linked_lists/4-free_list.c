aea62bb8660bb9188b390a6e9a39a4cd2af6efee
HanMohamed <hmohamed2@seattleu.edu>
2023-03-31 22:34:14 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head to a linked list
+ *
+ * a function that frees a list_t list.
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while (head->next != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+	free(head->str);
+	free(head);
+}
