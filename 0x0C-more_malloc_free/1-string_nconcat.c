5a50a1a5cc0dec5b8e31ebf4bfe4b667948c7a01
HanMohamed <hmohamed2@seattleu.edu>
2023-04-03 13:19:56 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (nconcat == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < ns1; i++)
 		nconcat[i] = s1[i];
 
 	for (i = 0; i < ns2; i++)
