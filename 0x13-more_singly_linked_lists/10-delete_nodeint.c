2353fc090a26d9b17649c5a05f94217385ffcdc6
HanMohamed <hmohamed2@seattleu.edu>
2023-04-04 01:05:18 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		prev = current;
 		current = current->next;
-		if (prev == NULL)
+		if (current == NULL)
 			return (-1);
 		i++;
 	}
