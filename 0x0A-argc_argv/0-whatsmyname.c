3f2dc4b8a68da57cfc1b8e76aeaa34676adc8d33
HanMohamed <hmohamed2@seattleu.edu>
2023-06-09 19:31:41 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,10 @@
  *
  * Return: name of file
  */
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
