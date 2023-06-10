a958a09f9eea90c9f045ed0697f8c3d7c07ab90f
HanMohamed <hmohamed2@seattleu.edu>
2023-06-09 21:06:41 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - Entry point
  * @argc: number of arguments
- * @argv: pointer
+ * @argv: pointer to arguments
  *
  * a program that add two numbers.
- * f the program does not receive two arguments,
- * program should print Error, followed by a new line, and return 1
+ * If no number is passed to the program, print 0 followed by a new line
+ * If one of the number contains symbols that are not digits, print Error
+ * followed by a new line, and return 1
  *
- * Return: name of file
+ * Return: 0 if successful
  */
 int main(int argc, char *argv[])
 {
-	int sum, n;
+	int sum, n, x;
+	char *p;
 
-	if (argc != 3)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
 
 	sum = 0;
+	p = NULL;
 	for (n = 1; n < argc; n++)
-		sum += atoi(argv[n]);
+	{
+		x = strtol(argv[n], &p, 10);
+		if (x == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += x;
+	}
 
 	printf("%d\n", sum);
 	return (0);
