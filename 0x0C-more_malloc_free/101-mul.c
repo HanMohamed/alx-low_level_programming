91f1e37dda0e20a1ecda05c0107fdbba4c18ced1
HanMohamed <hmohamed2@seattleu.edu>
2023-06-15 15:34:39 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,3 +1,59 @@
-//
-// Created by Hania Mohamed on 6/15/23.
-//
+#include "main.h"
+
+int check_isdigit(char *argv[]);
+
+/**
+ * A program that multiplies two positive numbers.
+ *
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: The multiplication of two numbers.
+ */
+int main (int argc, char *argv[])
+{
+	int i;
+
+	if (argc != 3 || check_isdigit(argv) != 0)
+	{
+		char *msg = "Error\n";
+		for (i = 0; msg[i]; i++)
+			_putchar(msg[i]);
+		exit(98);
+	}
+	return (0);
+}
+
+/**
+ * _isdigit - checks for a digit
+ * @c: integer character
+ *
+ * Description: a function that checks for a digit
+ *
+ * Return: 1 if c is digit otherwise 0.
+ *
+ * int _isdigit(int c)
+ * {
+ * 	if (c >= 48 && c <= 57)
+ * 		return (1);
+ * 	return (0);
+ * }
+*/
+
+/**
+ * check_isdigit - checks if argv are numbers;
+ * @argv: arguments
+ *
+ * Return: 0 if digit, if not it returns 1
+ */
+int check_isdigit(char *argv[])
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		j = 0;
+		if (argv[i][j] < 48 || argv[i][j] > 57)
+			return (1);
+	}
+	return (0);
+}
