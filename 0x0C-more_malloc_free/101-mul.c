3f3eb542a3d4187048aea48a010b5ad62bd9e116
HanMohamed <hmohamed2@seattleu.edu>
2023-06-30 11:02:22 -0700

    delete

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -24,22 +24,6 @@ int main (int argc, char *argv[])
 }
 
 /**
- * _isdigit - checks for a digit
- * @c: integer character
- *
- * Description: a function that checks for a digit
- *
- * Return: 1 if c is digit otherwise 0.
- *
- * int _isdigit(int c)
- * {
- * 	if (c >= 48 && c <= 57)
- * 		return (1);
- * 	return (0);
- * }
-*/
-
-/**
  * check_isdigit - checks if argv are numbers;
  * @argv: arguments
  *
