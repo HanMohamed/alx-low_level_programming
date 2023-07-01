aeacdf057a0646eefe3f138032dfb543acfa8d37
HanMohamed <hmohamed2@seattleu.edu>
2023-06-30 19:01:37 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,3 +1,24 @@
-//
-// Created by Hania Mohamed on 6/30/23.
-//
+#include "variadic_functions.h"
+
+int sum_them_all(const unsigned int n, ...)
+{
+	/* declare pointer to argument list */
+	va_list ap;
+	unsigned int i;
+	int sum;
+
+	if (n == 0)
+		return (0);
+
+	sum = 0;
+
+	/* Initializing argument to the list pointer */
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	/* Ending argument list traversal */
+	va_end(ap);
+
+	return (sum);
+}
