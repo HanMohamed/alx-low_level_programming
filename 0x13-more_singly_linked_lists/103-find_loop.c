e747bbd08545a386d48f4b4e56cbe9816e3c4bd7
HanMohamed <hmohamed2@seattleu.edu>
2023-07-05 18:04:52 -0700

    103-find_loop.c

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: head of list
+ *
+ * You can only declare a maximum of two variables in your function
+ *
+ * Return:  The address of the node where the loop starts,
+ * or NULL if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *fast, *slow;
+
+	if (!head)
+	return NULL;
+
+	slow = head;
+	fast = head->next->next;
+
+	while (fast != slow && fast && slow)
+	{
+		slow = slow->next;
+		fast = head->next->next;
+	}
+	if (fast != slow)
+		return (NULL);
+
+	fast = head;
+	while (fast != slow)
+	{
+		slow = slow->next;
+		fast = head->next;
+	}
+	return (fast);
+}
