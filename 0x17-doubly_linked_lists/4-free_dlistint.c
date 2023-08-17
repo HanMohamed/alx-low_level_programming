f7d3f06b2f8bc369829f695843fe4724d9238240
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-17 15:27:48 -0700

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,3 +1,21 @@
-//
-// Created by Hania Mohamed on 8/17/23.
-//
+#include "lists.h"
+
+/**
+ * free_dlistint -  frees a dlistint_t list.
+ * @head: doubly linked list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	if (!head)
+		return;
+
+	current = head;
+	while (current)
+	{
+		head = head->next;
+		free(current);
+		current = head;
+	}
+	free(head);
+}
