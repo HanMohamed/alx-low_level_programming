afcc8525b1911e0dbe45dc146e0f29cc867d4c79
Han Mohamed <hmohamed2@seattleu.edu>
2023-08-17 16:20:43 -0700

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,3 +1,25 @@
-//
-// Created by Hania Mohamed on 8/17/23.
-//
+#include "lists.h"
+
+/**
+ * sum_dlistint -  sum of all the data (n) of a dlistint_t linked lis
+ * @head:
+ * if the list is empty, return 0
+ * Return: returns the sum of all the data (n) of a dlistint_t linked list.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+
+	int sum = 0;
+	dlistint_t *current;
+
+	if (!head)
+		return (sum);
+
+	current = head;
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
