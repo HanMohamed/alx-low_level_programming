fbe7dbfb47f2265bcd25d94dec6ce91c8f25c2dc
Han Mohamed <hmohamed2@seattleu.edu>
2023-09-08 19:59:12 -0700

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *current;
 	unsigned long int i;
 
-	if (!ht)
+	if (!ht || !ht->array || ht->size == 0)
 		return;
 
 	for (i = 0; i < ht->size; i++)
