9c68b518e5a537913a34b6133cc167d5c4ffbc37
HanMohamed <hmohamed2@seattleu.edu>
2024-02-05 13:01:44 -0800

    0x1E-search_algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
