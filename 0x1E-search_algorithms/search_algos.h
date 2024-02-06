a36909952a7e0e4241deb87b34a771c3267b8575
HanMohamed <hmohamed2@seattleu.edu>
2024-02-05 16:00:14 -0800

    0x1E-search_algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,10 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchar(char c);
-
-void _puts_recursion(char *s);
-
 int linear_search(int *array, size_t size, int value);
 
 int binary_search(int *array, size_t size, int value);
